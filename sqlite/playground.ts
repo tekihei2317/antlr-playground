97a48ee61cec430697b6564292bb691e17b148e9
tekihei2317 <tekihei2317@gmail.com>
2023-05-30 12:07:26 +0900

    ColumnDefinition

--- sqlite/playground.ts
+++ sqlite/playground.ts
@@ -8,11 +8,22 @@ import SQLiteParser, {
 } from "./SQLiteParser";
 import SQLiteListener from "./SQLiteParserListener";
 
-type ColumnType = "INTEGER" | "TEXT" | "REAL" | "BLOB";
+const columnTypes = ["INT", "INTEGER", "TEXT", "REAL", "BLOB", "ANY"] as const;
+type ColumnType = (typeof columnTypes)[number];
+
+function toColumnType(type: string): ColumnType {
+  const types: readonly string[] = columnTypes;
+  if (types.includes(type.toUpperCase())) {
+    return type.toUpperCase() as ColumnType;
+  }
+  return "ANY";
+}
 
 type ColumnDefinition = {
   name: string;
   type: ColumnType;
+  nullable: boolean;
+  hasDefault: boolean;
 };
 
 type ConstraintType = "PRIMARY" | "UNIQUE";
@@ -31,20 +42,52 @@ type RelationDefinition = {
 
 type TableDefinition = {
   name: string;
-  // columns: ColumnDefinition[];
+  columns: ColumnDefinition[];
   // constraints: TableConstraint[];
   // relations: RelationDefinition[];
 };
 
+const tables: TableDefinition[] = [];
+
 class MyTreeWalker extends SQLiteListener {
   enterCreate_table_stmt = (ctx: Create_table_stmtContext) => {
-    console.log(ctx.table_name().getText());
-    console.log(
-      ctx.column_def_list().map((column) => column.column_name().getText())
-    );
-    console.log(
-      ctx.column_def_list().map((column) => column.type_name().getText())
-    );
+    for (const column of ctx.column_def_list()) {
+      const constraints = column.column_constraint_list();
+      for (const constraint of constraints) {
+        console.log(constraint.getText());
+      }
+    }
+
+    const columns: ColumnDefinition[] = ctx
+      .column_def_list()
+      .map((columnCtx) => {
+        const constraintTexts = columnCtx
+          .column_constraint_list()
+          .map((constraintCtx) => constraintCtx.getText());
+        const nullable =
+          constraintTexts.find((text) => text.toUpperCase() === "NOTNULL") ===
+          undefined;
+
+        // TODO: INTEGERでPRIMARY KEYの場合はAUTO_INCREMENTになるので、省略可能
+        // TODO: tableの制約でPRIMARY KEYが指定されている場合も考慮する
+        const hasDefault =
+          constraintTexts.find((text) =>
+            text.toUpperCase().startsWith("DEFAULT")
+          ) !== undefined;
+
+        const column: ColumnDefinition = {
+          name: columnCtx.column_name().getText(),
+          type: toColumnType(columnCtx.type_name().getText()),
+          nullable,
+          hasDefault,
+        };
+        return column;
+      });
+
+    tables.push({
+      name: ctx.table_name().getText(),
+      columns,
+    });
   };
 }
 
@@ -61,6 +104,8 @@ async function main() {
   const tree = parser.parse();
   const listener = new MyTreeWalker();
   ParseTreeWalker.DEFAULT.walk(listener, tree);
+
+  console.log(JSON.stringify(tables, null, 2));
 }
 
 main();
